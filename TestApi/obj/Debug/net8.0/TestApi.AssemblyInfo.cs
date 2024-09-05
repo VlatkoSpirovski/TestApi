01af39b3a8a44cb440d17a4e62ea6c7bccab4219
vlatkospirovski <spirovski.14@gmail.com>
2024-09-05 11:31:16 +0200

    Database

--- TestApi/obj/Debug/net8.0/TestApi.AssemblyInfo.cs
+++ TestApi/obj/Debug/net8.0/TestApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9a1a49c691a8a6ac5b03fd587d1a2725df91362")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f153a64561c74e857120347ebc625b7aa676586d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
